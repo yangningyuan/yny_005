dad5f31f85c74c9c7e4b645731e9ded35539c516
yangningyuan <yangningyuan@outlook.com>
2018-10-29 22:01:00 +0800

    1

---
+++ BLL/ObjExcel.cs
@@ -0,0 +1,169 @@
+﻿/**  版本信息模板在安装目录下，可自行修改。
+* ObjExcel.cs
+*
+* 功 能： N/A
+* 类 名： ObjExcel
+*
+* Ver    变更日期             负责人  变更内容
+* ───────────────────────────────────
+* V0.01  2018/10/29 21:42:43   N/A    初版
+*
+* Copyright (c) 2012 Maticsoft Corporation. All rights reserved.
+*┌──────────────────────────────────┐
+*│　此技术信息为本公司机密信息，未经本公司书面同意禁止向第三方披露．　│
+*│　版权所有：动软卓越（北京）科技有限公司　　　　　　　　　　　　　　│
+*└──────────────────────────────────┘
+*/
+using System;
+using System.Data;
+using System.Collections.Generic;
+using yny_005.Model;
+namespace yny_005.BLL
+{
+	/// <summary>
+	/// ObjExcel
+	/// </summary>
+	public partial class ObjExcel
+	{
+		
+		#region  BasicMethod
+
+		/// <summary>
+		/// 得到最大ID
+		/// </summary>
+		public int GetMaxId()
+		{
+			return DAL.ObjExcel.GetMaxId();
+		}
+
+		/// <summary>
+		/// 是否存在该记录
+		/// </summary>
+		public bool Exists(int ID)
+		{
+			return DAL.ObjExcel.Exists(ID);
+		}
+
+		/// <summary>
+		/// 增加一条数据
+		/// </summary>
+		public int  Add(yny_005.Model.ObjExcel model)
+		{
+			return DAL.ObjExcel.Add(model);
+		}
+
+		/// <summary>
+		/// 更新一条数据
+		/// </summary>
+		public bool Update(yny_005.Model.ObjExcel model)
+		{
+			return DAL.ObjExcel.Update(model);
+		}
+
+		/// <summary>
+		/// 删除一条数据
+		/// </summary>
+		public bool Delete(int ID)
+		{
+			
+			return DAL.ObjExcel.Delete(ID);
+		}
+		/// <summary>
+		/// 删除一条数据
+		/// </summary>
+		public bool DeleteList(string IDlist )
+		{
+			return DAL.ObjExcel.DeleteList(IDlist );
+		}
+
+		/// <summary>
+		/// 得到一个对象实体
+		/// </summary>
+		public yny_005.Model.ObjExcel GetModel(int ID)
+		{
+			
+			return DAL.ObjExcel.GetModel(ID);
+		}
+
+		
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public DataSet GetList(string strWhere)
+		{
+			return DAL.ObjExcel.GetList(strWhere);
+		}
+		/// <summary>
+		/// 获得前几行数据
+		/// </summary>
+		public DataSet GetList(int Top,string strWhere,string filedOrder)
+		{
+			return DAL.ObjExcel.GetList(Top,strWhere,filedOrder);
+		}
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public List<yny_005.Model.ObjExcel> GetModelList(string strWhere)
+		{
+			DataSet ds = DAL.ObjExcel.GetList(strWhere);
+			return DataTableToList(ds.Tables[0]);
+		}
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public List<yny_005.Model.ObjExcel> DataTableToList(DataTable dt)
+		{
+			List<yny_005.Model.ObjExcel> modelList = new List<yny_005.Model.ObjExcel>();
+			int rowsCount = dt.Rows.Count;
+			if (rowsCount > 0)
+			{
+				yny_005.Model.ObjExcel model;
+				for (int n = 0; n < rowsCount; n++)
+				{
+					model = DAL.ObjExcel.DataRowToModel(dt.Rows[n]);
+					if (model != null)
+					{
+						modelList.Add(model);
+					}
+				}
+			}
+			return modelList;
+		}
+
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public DataSet GetAllList()
+		{
+			return GetList("");
+		}
+
+		/// <summary>
+		/// 分页获取数据列表
+		/// </summary>
+		public int GetRecordCount(string strWhere)
+		{
+			return DAL.ObjExcel.GetRecordCount(strWhere);
+		}
+		/// <summary>
+		/// 分页获取数据列表
+		/// </summary>
+		public DataSet GetListByPage(string strWhere, string orderby, int startIndex, int endIndex)
+		{
+			return DAL.ObjExcel.GetListByPage( strWhere,  orderby,  startIndex,  endIndex);
+		}
+		/// <summary>
+		/// 分页获取数据列表
+		/// </summary>
+		//public DataSet GetList(int PageSize,int PageIndex,string strWhere)
+		//{
+			//return DAL.ObjExcel.GetList(PageSize,PageIndex,strWhere);
+		//}
+
+		#endregion  BasicMethod
+		#region  ExtensionMethod
+
+		#endregion  ExtensionMethod
+	}
+}
+
---
+++ BLL/ObjSample.cs
@@ -0,0 +1,169 @@
+﻿/**  版本信息模板在安装目录下，可自行修改。
+* ObjSample.cs
+*
+* 功 能： N/A
+* 类 名： ObjSample
+*
+* Ver    变更日期             负责人  变更内容
+* ───────────────────────────────────
+* V0.01  2018/10/29 21:42:43   N/A    初版
+*
+* Copyright (c) 2012 Maticsoft Corporation. All rights reserved.
+*┌──────────────────────────────────┐
+*│　此技术信息为本公司机密信息，未经本公司书面同意禁止向第三方披露．　│
+*│　版权所有：动软卓越（北京）科技有限公司　　　　　　　　　　　　　　│
+*└──────────────────────────────────┘
+*/
+using System;
+using System.Data;
+using System.Collections.Generic;
+using yny_005.Model;
+namespace yny_005.BLL
+{
+	/// <summary>
+	/// ObjSample
+	/// </summary>
+	public partial class ObjSample
+	{
+		
+		#region  BasicMethod
+
+		/// <summary>
+		/// 得到最大ID
+		/// </summary>
+		public int GetMaxId()
+		{
+			return DAL.ObjSample.GetMaxId();
+		}
+
+		/// <summary>
+		/// 是否存在该记录
+		/// </summary>
+		public bool Exists(int ID)
+		{
+			return DAL.ObjSample.Exists(ID);
+		}
+
+		/// <summary>
+		/// 增加一条数据
+		/// </summary>
+		public int  Add(yny_005.Model.ObjSample model)
+		{
+			return DAL.ObjSample.Add(model);
+		}
+
+		/// <summary>
+		/// 更新一条数据
+		/// </summary>
+		public bool Update(yny_005.Model.ObjSample model)
+		{
+			return DAL.ObjSample.Update(model);
+		}
+
+		/// <summary>
+		/// 删除一条数据
+		/// </summary>
+		public bool Delete(int ID)
+		{
+			
+			return DAL.ObjSample.Delete(ID);
+		}
+		/// <summary>
+		/// 删除一条数据
+		/// </summary>
+		public bool DeleteList(string IDlist )
+		{
+			return DAL.ObjSample.DeleteList(IDlist );
+		}
+
+		/// <summary>
+		/// 得到一个对象实体
+		/// </summary>
+		public yny_005.Model.ObjSample GetModel(int ID)
+		{
+			
+			return DAL.ObjSample.GetModel(ID);
+		}
+
+		
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public DataSet GetList(string strWhere)
+		{
+			return DAL.ObjSample.GetList(strWhere);
+		}
+		/// <summary>
+		/// 获得前几行数据
+		/// </summary>
+		public DataSet GetList(int Top,string strWhere,string filedOrder)
+		{
+			return DAL.ObjSample.GetList(Top,strWhere,filedOrder);
+		}
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public List<yny_005.Model.ObjSample> GetModelList(string strWhere)
+		{
+			DataSet ds = DAL.ObjSample.GetList(strWhere);
+			return DataTableToList(ds.Tables[0]);
+		}
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public List<yny_005.Model.ObjSample> DataTableToList(DataTable dt)
+		{
+			List<yny_005.Model.ObjSample> modelList = new List<yny_005.Model.ObjSample>();
+			int rowsCount = dt.Rows.Count;
+			if (rowsCount > 0)
+			{
+				yny_005.Model.ObjSample model;
+				for (int n = 0; n < rowsCount; n++)
+				{
+					model = DAL.ObjSample.DataRowToModel(dt.Rows[n]);
+					if (model != null)
+					{
+						modelList.Add(model);
+					}
+				}
+			}
+			return modelList;
+		}
+
+		/// <summary>
+		/// 获得数据列表
+		/// </summary>
+		public DataSet GetAllList()
+		{
+			return GetList("");
+		}
+
+		/// <summary>
+		/// 分页获取数据列表
+		/// </summary>
+		public int GetRecordCount(string strWhere)
+		{
+			return DAL.ObjSample.GetRecordCount(strWhere);
+		}
+		/// <summary>
+		/// 分页获取数据列表
+		/// </summary>
+		public DataSet GetListByPage(string strWhere, string orderby, int startIndex, int endIndex)
+		{
+			return DAL.ObjSample.GetListByPage( strWhere,  orderby,  startIndex,  endIndex);
+		}
+		/// <summary>
+		/// 分页获取数据列表
+		/// </summary>
+		//public DataSet GetList(int PageSize,int PageIndex,string strWhere)
+		//{
+			//return DAL.ObjSample.GetList(PageSize,PageIndex,strWhere);
+		//}
+
+		#endregion  BasicMethod
+		#region  ExtensionMethod
+
+		#endregion  ExtensionMethod
+	}
+}
+
